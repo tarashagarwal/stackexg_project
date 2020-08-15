eec7e2298623e7556bc0f8ae158f830eec79d6dc
tarashagarwal <tarash.agarwal@gmail.com>
2020-08-15 20:57:14 +0530

    Reducing the number Api Calls

--- app/app/controllers/stackexg/questions_controller.rb
+++ app/app/controllers/stackexg/questions_controller.rb
@@ -35,23 +35,40 @@ class Stackexg::QuestionsController < Spree::Api::BaseController
 		require 'open-uri'
 		questions_query = "https://api.stackexchange.com/2.2/questions?page=1&pagesize=10&fromdate=1597449600&todate=1598054400&order=desc&sort=#{sort}&tagged=#{search_keyword}&site=stackoverflow&auth_token=#{cookies[:stackexg_oauthtoken]}"
 		debugger
-		response    = open(questions_query).read
-		json_result = JSON.parse(response)
-		questions   = []
+		response        = open(questions_query).read
+		json_result     = JSON.parse(response)
+		questions       = []
+		question_ids    = []
+		question_titles = []
 		json_result["items"].each_with_index do |item,i|
-			next if item["answers_count"] == 0
-			temp_item = []
-			temp_item << item["title"].capitalize
-			answers_query 	 = "https://api.stackexchange.com/2.2/questions/#{item["question_id"]}?order=desc&sort=activity&site=stackoverflow&filter=!E-PH4Kvk4lz6dx697PeHVXbH8._5NUJXUwyenL&auth_token=#{cookies[:stackexg_oauthtoken]}"
-			answers    		 = open(answers_query).read
-			answers_response = JSON.parse(answers)
-			answers = []
-			answers_response["items"].each_with_index do |item,i|
-				answers << item["body"]
-			end
-			temp_item << answers
-			questions << temp_item
+			question_titles << item["title"].capitalize
+			question_ids    << item["question_id"]
 		end
+
+
+		answers_query 	 = "https://api.stackexchange.com/2.2/questions/#{question_ids.join(";")}?order=desc&sort=activity&site=stackoverflow&filter=!E-PH4Kvk4lz6dx697PeHVXbH8._5NUJXUwyenL&auth_token=#{cookies[:stackexg_oauthtoken]}"
+		answers    		 = open(answers_query).read
+		answers_response = JSON.parse(answers)
+		answer_items     = answers_response["items"]
+debugger
+		q_hash ={}
+
+		answer_items.each_with_index do |a_item,i|
+			if !a_item["body"].blank?
+				if q_hash[a_item["question_id"]].blank? 
+					q_hash[a_item["question_id"]]  = []
+				end
+				q_hash[a_item["question_id"]] = (q_hash[a_item["question_id"]] << a_item["body"])
+			end
+		end
+
+		question_ids.each_with_index do |id,i|
+			temp_arr = []
+			temp_arr << question_titles[i]
+			temp_arr << q_hash[id] if !q_hash[id].blank?
+			questions << temp_arr
+		end
+debugger
 		return questions
 	end
 end
